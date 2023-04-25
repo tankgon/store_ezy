01dfdde5638423a505a6b17596bd841f59159e51
Kyo Huu <phan.huu.chi.105@gmail.com>
2023-04-25 12:34:57 +0700

    refactor(Core): Change app icon, launcher icon

--- android/app/src/main/kotlin/com/ezstore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ezstore.mulstore
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/mulstorebb/smatez/com/MainActivity.kt
@@ -0,0 +1,6 @@
+package mulstorebb.smatez.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
