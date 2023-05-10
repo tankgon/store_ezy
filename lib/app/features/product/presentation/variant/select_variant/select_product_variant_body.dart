8e83e3927ad5c7eae34fe1e9808a6e2553e4474f
Kyo Huu <chi.ph@carehealth.io>
2023-05-10 16:20:01 +0700

    refactor(Cart): Increase click area for counter button

--- lib/app/features/product/presentation/variant/select_variant/select_product_variant_body.dart
+++ lib/app/features/product/presentation/variant/select_variant/select_product_variant_body.dart
@@ -40,7 +40,7 @@ class SelectProductVariantBody extends StatelessWidget {
               ),
             ),
           ],
-        ).pDefault(),
+        ).pxDefault().py12(),
       ],
     );
   }
